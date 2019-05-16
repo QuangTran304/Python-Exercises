221e064a00916525174ec1461dbd627fd5766fb2
Quang Tran <nhatquang304@gmail.com>
2019-05-15 23:04:24 -0400

    Exercise-15 completed

--- Py_Exercise-15.py
+++ Py_Exercise-15.py
@@ -4,5 +4,19 @@
 # -------------------------------------------------------------------------
 
 """
-
+Question:
+Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+Suppose the following input is supplied to the program:
+9
+Then, the output should be:
+11106
 """
+
+
+d = input("Please enter a digit: ")
+dd = str(d) + str(d)
+ddd = str(d) + str(d) + str(d)
+dddd = str(d) + str(d) + str(d) + str(d)
+
+result = int(d) + int(dd) + int(ddd) + int(dddd)
+print("Result is: " + str(result))
